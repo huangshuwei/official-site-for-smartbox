4371213f021104e04627c698fe4887a1e77b017a
huangshuwei <hsw89520@163.com>
2016-09-21 18:21:51 +0800

    修改

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,33 @@
 var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
     uglifyCss = require('gulp-minify-css'),
+    concat = require('gulp-concat'),
     //pump = require('pump'), // 可以截获错误
     rename = require('gulp-rename');
 
 
-/*
+/*/!*
  * 压缩业务js
- * */
+ * *!/
 gulp.task('compressjs1', function () {
-    gulp.src('src/js/*.js')
+    gulp.src('src/js/!*.js')
         .pipe(uglify())
         //给压缩后的文件，添加min后缀名
         .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest('dist/js'))
-});
+});*/
 
 
 /*
-/!*
  * 压缩公共js
- * *!/
+ * */
 gulp.task('compressjs2', function () {
-    gulp.src('public/js/!*.js')
+    gulp.src('public/js/*.js')
         .pipe(uglify())
         //给压缩后的文件，添加min后缀名
         .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest('public/js'))
 });
-*/
 
 
 /*
@@ -42,15 +41,41 @@ gulp.task('compresscss1', function () {
         .pipe(gulp.dest('dist/css'))
 });
 
-/*/!*
+/*
  * 压缩公共css
- * *!/
+ * */
 gulp.task('compresscss2', function () {
-    gulp.src('public/css/!*!/!*.css')
+    gulp.src('public/css/*/*.css')
         .pipe(uglifyCss())
         //给压缩后的文件，添加min后缀名
         .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest('public/css'))
-});*/
+});
 
-gulp.task('default', ['compressjs1','compresscss1']);
+
+/*合并js并压缩*/
+gulp.task('concat-compressjs', function () {
+    gulp.src(['src/js/examples.js','public/js/jquery.smartbox.js','public/js/prettify.js'])
+        .pipe(concat('libs_examples.js'))
+        .pipe(uglify())
+        //给压缩后的文件，添加min后缀名
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('dist/js'))
+});
+
+/*合并css并压缩*/
+gulp.task('concat-compresscss', function () {
+    gulp.src(['src/css/main.css','public/css/prettify/prettify.css','public/css/smartbox/jquery.smartbox.css'])
+        .pipe(concat('libs_examples.css'))
+        .pipe(uglifyCss())
+        //给压缩后的文件，添加min后缀名
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('dist/css'))
+});
+
+gulp.task('css_public',['compresscss2']);
+
+gulp.task('js_public',['compressjs2']);
+
+
+gulp.task('default', ['compresscss1','concat-compressjs','concat-compresscss']);
